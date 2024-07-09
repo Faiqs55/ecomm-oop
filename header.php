a2d2388769c2a9c4de2dcaefbaceb9130e38a7f0
Faiq Sindhu <sindhufaiq555@gmail.com>
2024-07-09 10:20:08 +0500

    working on home page

---
+++ header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles/style.css">
+    <script src="https://kit.fontawesome.com/3fda0b5bca.js" crossorigin="anonymous" defer></script>
+    <title>Ecomm | HOME</title>
+</head>
+<body>
+    <header class="main">
+            <!-- NAVBAR HTML  -->
+    <nav>
+        <!-- NAV LINKS  -->
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">Shop</a></li>
+            <li><a href="">About Us</a></li>
+            <li><a href="">Contact Us</a></li>
+        </ul>
+
+        <!-- LOGO  -->
+        <h1>
+            <span class="sm">OOP</span>
+            <span class="lg">Ecomm</span>
+        </h1>
+
+        <!-- BUTTONS -->
+         <div>
+          <a href="">
+          <i class="fa-solid fa-magnifying-glass"></i>
+          </a>
+          <a href="">
+          <i class="fa-solid fa-bag-shopping"></i>
+          </a>
+         </div>
+    </nav>
+    </header>
---
+++ index.php
@@ -0,0 +1,63 @@
+<?php include "./header.php" ?>
+
+
+ <main class="hero">
+
+    <div class="title">
+      <h3 class="sm">
+      CASUAL & EVERYDAY
+      </h3>
+      <h1 class="lg">
+      Effortlessly Blend Comfort <br> & Style!
+      </h1>
+    </div>
+
+    <p>Effortlessly blend comfort and style with our Casual & Everyday collection, featuring cozy sweaters, versatile denim, laid-back tees, and relaxed-fit joggers for your everyday adventures</p>
+
+    <a href="">
+        VIEW COLLECTION
+    </a>
+ </main>
+
+ <section class="popular">
+     <h2>Most Popular</h2>
+
+     <div class="container">
+        <div class="card">
+        <div class="img">
+           <img src="./admin/img/kurti.jpg" alt="">
+           </div>
+           <h3 class="category">CATEGORY</h3>
+           <h3 class="title">TITLE OF THE ITEM</h3>
+           <h3 class="price">$PRICE</h3>
+        </div>
+        <div class="card">
+        <div class="img">
+           <img src="./admin/img/hood.jpg" alt="">
+           </div>
+           <h3 class="category">CATEGORY</h3>
+           <h3 class="title">TITLE OF THE ITEM</h3>
+           <h3 class="price">$PRICE</h3>
+        </div>
+        <div class="card">
+        <div class="img">
+           <img src="./admin/img/sunglasses.jpg" alt="">
+           </div>
+           <h3 class="category">CATEGORY</h3>
+           <h3 class="title">TITLE OF THE ITEM</h3>
+           <h3 class="price">$PRICE</h3>
+        </div>
+        <div class="card">
+        <div class="img">
+           <img src="./admin/img/top.jpg" alt="">
+           </div>
+           <h3 class="category">CATEGORY</h3>
+           <h3 class="title">TITLE OF THE ITEM</h3>
+           <h3 class="price">$PRICE</h3>
+        </div>
+     </div>
+
+ </section>
+
+</body>
+</html>
