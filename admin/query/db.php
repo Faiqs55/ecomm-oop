aaa8a6416152495f2ee1a6b070b00a1b7bad221f
Faiq Sindhu <sindhufaiq555@gmail.com>
2024-07-12 16:44:14 +0500

    admin class complete

--- admin/query/db.php
+++ admin/query/db.php
@@ -1,5 +1,10 @@
 <?php 
 
+session_start();
+if(!$_SESSION['admin']){
+    header("Location: http://localhost/ecomm.oop/admin");
+}
+
 class db{
     private $hostname = "localhost";
     private $username = "root";
@@ -17,19 +22,82 @@ class db{
     }
 
     // ADD DATA TO DB 
-    function add($table, $params){
+    function add($table, $params, $type=null){
         if($this->check_table($table)){
            $cols = implode(", ", array_keys($params));
            $vals = implode("', '", $params);
-           $sql = "INSERT INTO $table($cols) VALUES('{$vals}')";
-           
-           if($this->mysql->query($sql)){
+           $this->sql = "INSERT INTO $table($cols) VALUES('{$vals}')";
+           if(!$type){
+            $this->sql .= "; UPDATE categories SET no_products = no_products + 1 WHERE c_id = {$params["p_category"]}";
+           }
+        //    echo $this->sql;
+        //    die();
+           if($this->mysql->multi_query($this->sql)){
                array_push($this->result, "Query successfull ('$vals') Inserted in Table");
            }
         }else{
             die("Please Enter a valid Table name");
         }
     }
+
+    // GET DATA FROM DATABASE 
+
+    function getData($table, $rows="*", $join=null, $clause=null, $order=null, $lim=null){
+       if($this->check_table($table)){
+          $this->sql = "SELECT $rows FROM $table";
+          if($join){
+            $this->sql .= " JOIN $join";
+          }
+          if($clause){
+            $this->sql .= " WHERE $clause";
+          }
+          if($order){
+            $this->sql .= " ORDER BY $order";
+          }
+          if($lim){
+            $this->sql .= " LIMIT $lim";
+          }
+          
+          $res = $this->mysql->query($this->sql);
+          if($res->num_rows > 0){
+            $this->result = $res->fetch_all(MYSQLI_ASSOC);
+            
+          }
+
+       }else{
+        die("Please enter a valid table name");
+       }
+    }
+
+    // UPDATE DATA FROM DATABASE 
+    function updateData($table, $params=array(), $clause){
+        if($this->check_table($table)){
+            // $cols = implode(", ", array_keys($params));
+            // $vals = implode("', '", $params);
+            $vals = '';
+            foreach ($params as $key => $value) {
+                $vals = "$key = '$value'";
+            }
+            $this->sql = "UPDATE $table SET $vals $clause";
+            if($this->mysql->query($this->sql)){
+                array_push($this->result, "Update ($vals) $clause");
+            }
+        }else{
+            die("ENTER A VALID TABLE NAME");
+        }
+    }
+
+
+    // DELETE FROM DATABASE 
+    function deleteData($table, $clause){
+        if($this->check_table($table)){
+            $this->sql = "DELETE FROM $table $clause";
+            if($this->mysql->query($this->sql)){
+              array_push($this->result, "Deleted From database $clause");
+            }
+        }
+    }
+
 
     // CHECK IF TABLE EXISTS 
     function check_table($table){
@@ -62,5 +130,11 @@ class db{
 
 $obj = new db();
 
-$obj->add("admin", ["name"=> "Faiq", "pass"=> "mango"]);
-$obj->getRes();
+$title = "Men's Jacket";
+$des = "Lorem ipsum";
+$price = "200";
+$size = "XS, S, M";
+$color = "Red";
+$id = "3";
+
+
